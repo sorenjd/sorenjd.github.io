ead5cf211ff222cd9ed814755b673a366183f15c
Sören Dwelk <soeren.dwelk@stud.h-da.de>
2021-12-10 14:46:53 +0100

    newJS

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+"use strict";
 function helloWorld() {
     document.getElementById('handle').textContent =
     'D³SYMPL'
 }
+
+
+let request = new XMLHttpRequest();
+function requestData(){
+    window.alert("CookieData");
+    // request.open("GET","zeit.php");
+    // request.onreadystatechange = processData;
+    // request.send(null);
+}
+
 helloWorld();
-window.alert("Cookie1");
